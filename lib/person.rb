9ea94e948271ea76727334cd496e7ceaeadc28c3
Scott Weinstein <sweinstein27@gmail.com>
2018-04-23 14:50:45 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  def Person
+    @name
+  end
+
+  def Person=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+end
+ 
+    
